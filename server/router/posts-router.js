9ab4d124da4da5dbc5af9b7ae708beafe64fd647
TaigaShiga <gaitatg97@gmail.com>
2023-04-21 21:05:51 +0200

    feat: add image to posts folder

--- server/router/posts-router.js
+++ server/router/posts-router.js
@@ -6,14 +6,29 @@ const cloudinary = require("../../cloudinary.conf");
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+// upload image 
+async function handleUpload(file) {
+  const res = await cloudinary.uploader.upload(file, {
+    folder: 'posts',
+    resource_type: "auto",
+   });
+  return res;
+}
+
 router.post("/", async (req, res) => {
   console.log(req.body);
 });
 
 router.post("/uploadImage", upload.single('image'), async (req, res, next) => {
   const file = req.file;
-  console.log(cloudinary);
-  console.log("you are in /uploadImage",file);
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURL = "data:" + req.file.mimetype + ";base64," + b64;
+    let response = await handleUpload(dataURL);
+    console.log("🟩",response);
+  } catch (err){ 
+    console.log("🟥",err);
+  }
 });
 
 module.exports = router;
